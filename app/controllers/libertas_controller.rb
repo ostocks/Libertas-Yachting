c5436de6ed350b02cb86988c90b921851ee369ad
User <mrstocks@sd-10197.(none)>
2009-06-15 02:13:01 +0200

    Added plugin active scaffold methods for getting the testimonials

--- app/controllers/libertas_controller.rb
+++ app/controllers/libertas_controller.rb
@@ -1,17 +1,21 @@
 class LibertasController < ApplicationController
   
-  ##
-  # Lbertas Yachting
-  #
-  # Author	Stocks Oliver <stocks.oliver@gmail.com>
-  # Date 	Sat Jun 13 16:17:15 CEST 2009
-  #
-  # Description	Main action for libertas yachting, as this is a one page site
-  #		Expect lots of Ajax and private method as all will be called
-  #		from the index action.
-  #
-  ##
+  public
+    ##
+    # Lbertas Yachting
+    #
+    # Author	Stocks Oliver <stocks.oliver@gmail.com>
+    # Date 	Sat Jun 13 16:17:15 CEST 2009
+    #
+    # Description	Main action for libertas yachting, as this is a one page site
+    #		Expect lots of Ajax and private method as all will be called
+    #		from the index action.
+    #
+    ##
 
-  def index
-  end
+    def index
+      ##
+      # Last testimonials 
+      @testimonials = Testimonial.get_last_testimonials
+    end
 end
--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,2 +1,12 @@
 class Testimonial < ActiveRecord::Base
+
+  public
+    # Random int
+    @i = 0
+  
+    ## Get the last testimonials
+    def self.get_last_testimonials
+      self.find(:all, :limit => "3", :order => "created_at DESC")
+    end
+  
 end
